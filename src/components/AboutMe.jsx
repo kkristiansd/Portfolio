fb93f89f434c83c93743e13d7390c6bd6c683c7a
gilbertTheProgrammer <325178@VIAUC.DK>
2023-03-12 12:02:58 +0100

    About me section skeleton

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,36 @@
-const AboutMe =()=>{
+import Image from "next/image";
+import React from "react";
 
-    //return()
-        
-        
-    
-}
-export default AboutMe
+const AboutMe = () => {
+  return (
+    <div className="p-8 bg-transparent">
+      <div className="grid grid-cols-2">
+        <div className="p-4 flex item-center justify-center bg-transparent">
+          <p>
+            <p class="italic text-base font-normal leading-loose">
+              Hey, I'm Kris
+            </p>
+            <p className="text-2xl font-semibold leading-relaxed indent-8">
+              I'm a Software Technology Engineering student from Latvia. I'm a
+              team player, motivated, and goal-oriented. My passion for software
+              engineering is fueled by the belief that technology can transform
+              the world. In my free time, I enjoy exploring new technologies and
+              experimenting with programming languages. Thank you for visiting
+              my portfolio website!
+            </p>
+          </p>
+        </div>
+        <div className="p-4 flex item-center justify-center bg-transparent ">
+          <Image className=" transform -scale-x-100 rounded-full border border-transparent shadow-sm relative w-500 h-300"
+            src="/../public/assets/myAvatar (1).png"
+            alt="/"
+            width="300"
+            height="30"
+            class="transform -scale-x-100"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AboutMe;
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,6 +1,6 @@
 const Background = () => {
   return (
-    <div>
+    <div className="">
       <div class="stars">
         <div class="star" ></div>
         <div class="star"></div>
